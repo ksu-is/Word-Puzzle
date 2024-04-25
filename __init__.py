2a812a13068d8a6cd077dd2439285dcee299bffe
kdonjuan <kdonjuan@students.kennesaw.edu>
2024-04-25 17:13:39 -0400

    update
    added the flask and app so the webpage can work with the crossword puzzle.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
+import os
+import functools
+import random
 
-
+from wordsearch import WordSearch
+from flask import Flask, render_template, request, jsonify
 
 def create_app():
     #create and configure the app
@@ -31,4 +35,20 @@ def create_app():
     
     @app.route('/word-search', methods=['GET'])
     def word_search_page(): 
-    
+        word_list = random.sample(random_words, 10)
+        word_search = WordSearch()
+        word_search.generate(word_list)
+        grid = word_search.grid
+        return render_template('pages/word-search.html', grid=grid, words=word_list)
+
+@app.route('/check_match', methods=['POST'])
+def check_math():
+    word = request.json.get('word')
+    math_found = check_match_with_word_list(word)
+    return jonsify({'match_found' : match_found})
+
+def check_match_with_word_list(word):
+    return word in random_words
+
+return create_app
+
