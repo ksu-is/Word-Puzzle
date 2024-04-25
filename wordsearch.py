fe085ebd7ca7d63593836173a68afc1bee51b56a
kdonjuan <kdonjuan@students.kennesaw.edu>
2024-04-25 16:56:52 -0400

    update wordsearch.py
    Some of the red had to be improved in order for the crossword to work. Then some of the red got sent to the init file. In green I added the directions the crossword can be solved.

--- wordsearch.py
+++ wordsearch.py
@@ -19,63 +19,49 @@ class WordSearch:
 
         self.fill_remaining()
 
+
+# the line below is the directions to do the crossword puzzle
+    def place_words(self):
+        directions = [(1, 0), (0, 1), (1, 1), (-1, 1)] 
+        for word in self.words:
+            placed = Flase
+            while not placed:
+                direction = random.choice(directions)
+                start_row = random.randint(0, self.size - 1)
+                start_col = random.randint(0, self.size - 1)
+
+                end_row = start_row + len(word) * direction[0]
+                end_col = start_col = len(word) * direction[1]
+
+                if 0 <= end_row < self.size and 0 <= end_col < self.size:
+                    valid_placement = True
+                    for i in range (len(word)): 
+                        row = start_row + i * direction[0]
+                        col = start_col + i * direction[1]
+                        if self.grid[row][col] != '':
+                            valid_placement = False
+                            break
+                        if valid_placement:
+                            for i in range(len(word)):
+                                row = start_row + i * direction[0]
+                                col = start_col + i * direction[1]
+                                self.grid[row][col] = word[i]
+                            placed = True
+
+
+
+def fil_ramaining(self):
+    letters = string.ascoo_uppercase
+
+    for i in range(self.size):
+
+        for j in range(self.size):
     
-            
-    
+            if self.grid[i][j] == '':
+                self.grid[i][j] == random.choice(letters)
 
 
+    def display(self):
+        for row in self.grid:        
+            print(''.join(row))
 
-
-# Put the words you are trying to find here. All the text you enter should be 100% uppercase or 100% lowercase.
-
-solutions = ['PYTHON', 'ROBBIE', 'GITHUB', 'BEEF']
-
-# Here goes your puzzle, separate each letter by a space.
-
-puzzle ='''B P G S B S O R
-U D Y B E E F O
-H B W T E R G B
-T Z C N H D E B
-I E E W Z O S I
-G C H A N B N E
-'''
-
-# Just formats the puzzle into a more computer-readable text
-wordgrid = puzzle.replace(' ','')
-
-# Computers start counting at zero, so...
-length = wordgrid.index('\n')+1
-
-
-characters = [(letter, divmod(index, length))
-            for  index, letter in enumerate (wordgrid)]
-
-wordlines = {}
-# These next lines just  directions so you can tell which direction the word is going
-directions = {'going downwards':0, 'going downwards and left diagonally':-1, 'going downwards and right diagonally':1}
-
-for word_direction, directions in directions.items():
-    wordlines[word_direction] = []
-    for x in range(length):
-        for i in range(x, len(characters), length + directions):
-            wordlines[word_direction].append(characters[i])
-        wordlines[word_direction].append('\n')
-
-# Nice neat way of doing reversed directions.
-wordlines['going right'] = characters
-wordlines['going left'] = [i for i in reversed(characters)]
-wordlines['going upwards'] = [i for i in reversed(wordlines['going downwards'])]
-wordlines['going upwards and left diagonally'] = [i for i in reversed(wordlines['going downwards and right diagonally'])]
-wordlines['going upwards and right diagonally'] = [i for i in reversed(wordlines['going downwards and left diagonally'])]
-
-
-def printitout(direction, tuple, lines):
-    print "Keep in mind, rows are horizontal and columns are vertical.\n"
-    for direction, tuple in lines.items():
-        string = ''.join([i[0] for i in tuple])
-        for word in solutions:
-            if word in string:
-                coordinates = tuple[string.index(word)][1]
-                print word, 'is at row', coordinates[0]+1, 'and column', coordinates[1]+1, direction + "."
-
-printitout(word_direction, tuple, wordlines)
